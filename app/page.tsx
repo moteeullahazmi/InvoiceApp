213a4c3e5b0c5dcf21bee6ca72e7d8d6ee3cc627
moteeullahazmi <moteeullahazmi@gmail.com>
2025-01-17 18:43:21 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,32 @@
+import { Label } from "@/components/ui/label"
+import {Card, CardContent, CardDescription, CardHeader, CardTitle} from './../components/ui/card'
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
     <>
-    main root page
+   <div className="h-screen w-full flex justify-center items-center ">
+        <Card className="max-w-sm">
+            <CardHeader>
+                <CardTitle className="text-2xl">
+                    Login
+                </CardTitle>
+                <CardDescription>
+                    Enter your email below to login in to your account
+                </CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form className="flex flex-col gap-y-4">
+                    <div className="flex flex-col gap-y-2">
+                        <Label>Email</Label>
+                        <Input placeholder="azmi@gmail.com"/>
+                        <Button>Submit</Button>
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
+    </div>
     </>
   );
 }
