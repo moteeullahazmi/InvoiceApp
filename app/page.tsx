9fa5f450ac9ab4c4b27f2d1da702fd83ce8f6017
moteeullahazmi <moteeullahazmi@gmail.com>
2025-01-20 21:06:38 +0530

    change

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import { Label } from "@/components/ui/label"
-import {Card, CardContent, CardDescription, CardHeader, CardTitle} from './../components/ui/card'
-import { Input } from "@/components/ui/input"
-import { Button } from "@/components/ui/button"
+
 
 export default function Home() {
   return (
     <>
    <div className="h-screen w-full flex justify-center items-center ">
-       <h1>Working .... </h1>
+       /login api working
     </div>
     </>
   );
